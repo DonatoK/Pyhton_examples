202cf0a78d55dcc6f9545e2a9445437c0f9e1768
donatok <donatokava@gmail.com>
2023-02-04 07:33:55 -0500

    chapters 1 and two

---
+++ ch_1_and_2.py
@@ -0,0 +1,46 @@
+# chapter 1 topics
+# What is a program
+# Running Python
+# the first program
+# Arithmetic operators
+# values and types
+# formal and natural languages
+# debugging
+# assignments
+# variable names
+# expressions and statements
+# script mode
+# order of operations
+# comments
+# debugging
+
+
+
+
+
+print("hello World!")
+
+# I will add prints
+
+# A string value
+String = "why not"
+# integer values
+value = 5
+another_value = 7
+two = 2
+# float  value
+pi = 3.14
+
+new_value = value + another_value
+
+new_value -= pi
+
+power_o_2 = two ** 2
+
+
+# update in a loop
+
+while value > 5 :
+#    print(value)
+    value = value -1
+#print("endloop")
